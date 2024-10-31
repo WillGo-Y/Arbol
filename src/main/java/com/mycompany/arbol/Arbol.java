aee3dac9b452e8acef63b0168acd168367ab1324
WIN11 <WIN11@DESKTOP-IN5KN6F>
2024-10-31 14:42:07 -0500

    inicial

---
+++ src/main/java/com/mycompany/arbol/Arbol.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arbol;
+
+/**
+ *
+ * @author WIN11
+ */
+public class Arbol {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
